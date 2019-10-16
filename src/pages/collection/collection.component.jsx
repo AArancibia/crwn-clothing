afd6ced357a50525eb6a49568edc58006919c86c
Alexis Arancibia <alexis2396@hotmail.com>
2019-10-16 12:30:47 -0500

    moved our directory and shop data into its own reducer, created respective selectors and update corresponding components with redux flow for directory/shop

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <div className="title">{title}</div>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+      {/* <CollectionPreview title={title} items={items}></CollectionPreview> */}
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
