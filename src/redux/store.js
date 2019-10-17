0dd136af9163e7bd4ccc2467ffc444470ebadd21
Alexis Arancibia <alexis2396@hotmail.com>
2019-10-17 09:46:45 -0500

    Another2

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,6 +10,8 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
+console.log(process.env.NODE_ENV);
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
